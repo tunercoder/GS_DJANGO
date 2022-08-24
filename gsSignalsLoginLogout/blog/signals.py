3a151cc7486b61ffe0b30bc79f0ab5e2b195f801
Ashish Nautiyal <ashish.nautiyal@one97.delhi.net>
2022-08-24 10:34:13 +0530

    Signal file added model signals

--- gsSignalsLoginLogout/blog/signals.py
+++ gsSignalsLoginLogout/blog/signals.py
@@ -1,7 +1,9 @@
 from django.contrib.auth.signals import user_logged_in,user_logged_out,user_login_failed
+from django.db.models.signals import pre_delete,pre_init,pre_save,post_delete,post_init,post_save
 from django.contrib.auth.models import User
 from django.dispatch import receiver
 
+#auth signals 
 @receiver(user_logged_in,sender=User)
 def login_success(sender,request,user,**kwargs):
     print('user logged in success===================')
@@ -14,7 +16,7 @@ def login_success(sender,request,user,**kwargs):
 # user_logged_in.connect(login_success,sender=User)
 
 @receiver(user_logged_out,sender=User)
-def login_success(sender,request,user,**kwargs):
+def log_out(sender,request,user,**kwargs):
     print('user user_logged_out===================')
     print('sender details:::',sender)
     print('request is ::: ',request)
@@ -24,8 +26,65 @@ def login_success(sender,request,user,**kwargs):
 
 
 @receiver(user_login_failed)
-def login_success(sender,credentials,request,**kwargs):
+def login_failed(sender,credentials,request,**kwargs):
     print('user logged in failed===================')
     print('sender details:::',sender)
     print('request is ::: ',request)
     print('credentials is ::: ',credentials)
+
+
+
+#Model retated signals
+
+@receiver(pre_save,sender=User)
+def at_begining_save(sender,instance,**kwargs):
+    print('user model before save called for model user and signal pre_save===================')
+    print('sender details:::',sender)
+    print('instance is ::: ',instance)
+    print('keargs are::::',kwargs)
+
+
+@receiver(post_save,sender=User)
+def at_ending_save(sender,instance,created,**kwargs):
+    if created:
+        print('user model after save called new object created for model user and signal post_save===================')
+        print('sender details:::',sender)
+        print('request is ::: ',instance)
+        print('keargs are::::',kwargs)
+    else:
+        print('user model after save called but update run for model user and signal post_save===================')
+        print('sender details:::',sender)
+        print('request is ::: ',instance)
+        print('keargs are::::',kwargs)
+
+
+@receiver(pre_delete,sender=User)
+def at_begining_delete(sender,instance,**kwargs):
+    print('user model before delete called for model user and signal pre_detele===================')
+    print('sender details:::',sender)
+    print('instance is ::: ',instance)
+    print('keargs are::::',kwargs)
+
+
+@receiver(post_delete,sender=User)
+def at_ending_delete(sender,instance,**kwargs):
+    print('user model after delete called for model user and signal post_detele===================')
+    print('sender details:::',sender)
+    print('request is ::: ',instance)
+    print('keargs are::::',kwargs)
+
+
+@receiver(pre_init,sender=User)
+def at_begining_init(sender,*args,**kwargs):
+    print('user model before init called for model user and signal pre_init===================')
+    print('sender details:::',sender)
+    print('request is ::: ',args)
+    print('keargs are::::',kwargs)
+
+
+@receiver(post_init,sender=User)
+def at_ending_init(sender,*args,**kwargs):
+    print('user model after init called for model user and signal pre_init===================')
+    print('sender details:::',sender)
+    print('request is ::: ',args)
+    print('keargs are::::',kwargs)
