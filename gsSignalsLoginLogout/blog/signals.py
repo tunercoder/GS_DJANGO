8039f63a8b38749f9ef1c1ccb3f1a6e16a40aaff
Ashish Nautiyal <ashish.nautiyal@one97.delhi.net>
2022-08-24 11:22:29 +0530

    added Management Signals,Request/Response Signals,Database Wrappers

--- gsSignalsLoginLogout/blog/signals.py
+++ gsSignalsLoginLogout/blog/signals.py
@@ -1,5 +1,7 @@
 from django.contrib.auth.signals import user_logged_in,user_logged_out,user_login_failed
-from django.db.models.signals import pre_delete,pre_init,pre_save,post_delete,post_init,post_save
+from django.db.models.signals import pre_delete,pre_init,pre_save,post_delete,post_init,post_save,pre_migrate,post_migrate
+from django.core.signals import request_finished,request_started,got_request_exception
+from django.db.backends.signals import connection_created
 from django.contrib.auth.models import User
 from django.dispatch import receiver
 
@@ -88,3 +90,67 @@ def at_ending_init(sender,*args,**kwargs):
     print('sender details:::',sender)
     print('request is ::: ',args)
     print('keargs are::::',kwargs)
+
+
+
+#request retated signals
+
+@receiver(request_started)
+def at_request_started(sender,environ,**kwargs):
+    print(' before request_started called  and signal request_started===================')
+    print('sender details:::',sender)
+    print('environ details:::',environ)
+    print('keargs are::::',kwargs)
+
+
+@receiver(request_finished)
+def at_request_finished(sender,**kwargs):
+    print(' after request_finished  and signal request_finished===================')
+    print('sender details:::',sender)
+    print('keargs are::::',kwargs)
+
+@receiver(got_request_exception)
+def at_got_request_exception(sender,request,**kwargs):
+    print(' got_request_exception called and signal got_request_exception===================')
+    print('sender details:::',sender)
+    print('request is ::: ',request)
+    print('keargs are::::',kwargs)
+
+#management signals for migrations
+
+@receiver(pre_migrate)
+def at_pre_migrate(sender, app_config, verbosity, interactive, using, plan, apps,**kwargs):
+    print(' before pre_migrate called  and signal pre_migrate===================')
+    print('sender details:::',sender)
+    print('app_config details:::',app_config)
+    print('verbosity details:::',verbosity)
+    print('app_config details:::',app_config)
+    print('interactive are::::',interactive)
+    print('using details:::',using)
+    print('plan details:::',plan)
+    print('apps are::::',apps)
+    print('kwargs are::::',kwargs)
+
+
+@receiver(post_migrate)
+def at_post_migrate(sender, app_config, verbosity, interactive, using, plan, apps,**kwargs):
+    print(' after post_migrate  and signal post_migrate===================')
+    print('sender details:::',sender)
+    print('app_config details:::',app_config)
+    print('verbosity details:::',verbosity)
+    print('app_config details:::',app_config)
+    print('interactive are::::',interactive)
+    print('using details:::',using)
+    print('plan details:::',plan)
+    print('apps are::::',apps)
+    print('kwargs are::::',kwargs)
+
+
+#databse wrapper signals sent by the database wrapper when a database connection is initiated.
+
+@receiver(connection_created)
+def at_connection_created(sender,connection,**kwargs):
+    print(' before connection_created called  and signal connection_created===================')
+    print('sender details:::',sender)
+    print('connection details:::',connection)
+    print('kwargs are::::',kwargs)
--- gsSignalsLoginLogout/blog/views.py
+++ gsSignalsLoginLogout/blog/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.http.response import HttpResponse
 
 # Create your views here.
+def home(request):
+    a=10/0
+    return HttpResponse("hello")
